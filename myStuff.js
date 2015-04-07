26a7701e9f8a811cfef0454ef61d4c95ef2d95e4
Srinjita Bhaduri <bhaduri.srinjita@colorado.edu>
2015-04-07 10:23:42 -0600

    Adding files

---
+++ myStuff.js
@@ -0,0 +1,34 @@
+// Wrap everything in a function to keep your stuff private.
+(function () {
+
+    // JavaScript strict mode is a good thing.
+    "use strict";
+
+    // Define a unique global namespace for your stuff.
+    // You should change this to a namespace that is appropriate for your project.
+    fluid.registerNamespace("myStuff");
+
+    var enviro = flock.init();
+
+    // Expose any public functions or constructors as properties on your namesapce.
+    myStuff.play = function () {
+        // Triangle oscillator.
+    // Triggers a note whenever the mouse is clicked.
+    // Simple sine wave oscillator
+var synth = flock.synth({
+    synthDef: {
+        ugen: "flock.ugen.sinOsc",
+        freq: 440,
+        mul: 0.25
+    }
+});
+      
+
+   
+        // If you're on iOS, you will need to call in a listener for
+        // some kind of user input action, such a button click or touch handler.
+        // This is because iOS will only play sound if the user initiated it.
+        enviro.play();
+    };
+
+}());
