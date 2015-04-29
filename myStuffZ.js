c09e68087e7cf2837a6bc51d25e74d82ea608272
Srinjita <bhaduri.srinjita@colorado.edu>
2015-04-28 18:02:45 -0600

    Create myStuffZ.js

---
+++ myStuffZ.js
@@ -0,0 +1,36 @@
+// Wrap everything in a function to keep your stuff private.
+(function () {
+
+    // JavaScript strict mode is a good thing.
+    "use strict";
+
+    // Define a unique global namespace for your stuff.
+    // You should change this to a namespace that is appropriate for your project.
+    fluid.registerNamespace("myStuffZ");
+
+    var enviro = flock.init();
+
+    // Expose any public functions or constructors as properties on your namesapce.
+    myStuffZ.play = function () {
+
+        var synth = flock.synth({
+            synthDef: {
+                ugen: "flock.ugen.playBuffer", // example of a .wav file input
+                buffer:{
+                    id:"Z",
+                    url:"sounds/Z.wav",
+                } /*
+                ugen: "flock.ugen.sinOsc",
+                freq: 440,
+                duration: 1.0*/
+            }
+        });
+
+        enviro.play();
+    };
+
+    myStuffZ.end = function () {
+        enviro.stop();
+    }
+
+}());
